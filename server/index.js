19440c6227c760ea954e8a1b48d04676d04ae8cb
shubhamvinnovate <shubham.sutar@vinnovatetechnologies.com>
2025-06-17 22:06:48 +0530

    added cors and bosy parser middleware

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require("express")
 const app = express()
 require("dotenv").config()
-const port = process.env.PORT || 5000
+const cors = require("cors")
+const bodyParser = require("body-parser")
 const cart = require("./routes/cart")
 const product = require("./routes/product")
 
+const port = process.env.PORT || 5000
 const API_PREFIX = process.env.API_PREFIX || "api"
+
+app.use(cors())
+app.use(bodyParser.json({ type: "application/*+json" }))
 
 app.get("/", (req, res) => {
   res.send("Server is Running!")
