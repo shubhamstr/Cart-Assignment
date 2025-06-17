126dce190ad4e7becf6082072d0124745a7893d2
shubhamvinnovate <shubham.sutar@vinnovatetechnologies.com>
2025-06-17 19:31:41 +0530

    Header component added, installed material UI, products and cart route create

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,7 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes"
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/home.tsx"),
+  route("products", "routes/Products.tsx"),
+  route("cart", "routes/Cart.tsx"),
+] satisfies RouteConfig
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,19 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from "./+types/home"
+// import { Welcome } from "../welcome/welcome";
+import Header from "../Header/Index"
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
-  ];
+  ]
 }
 
 export default function Home() {
-  return <Welcome />;
+  // return <Welcome />;
+  return (
+    <>
+      <Header />
+    </>
+  )
 }
